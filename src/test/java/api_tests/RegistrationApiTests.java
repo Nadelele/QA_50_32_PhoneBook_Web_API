55b75a4353ceb19d12de09a6e82c4810ca5bcd94
Nadelele <nadele@gmail.com>
2026-03-11 18:53:06 +0200

    HomeWork16 - negative registration api tests added

--- src/test/java/api_tests/RegistrationApiTests.java
+++ src/test/java/api_tests/RegistrationApiTests.java
@@ -1,52 +1,85 @@
 package api_tests;
 
 import dto.User;
-import net.datafaker.idnumbers.SouthAfricanIdNumber;
-import okhttp3.OkHttpClient;
+import okhttp3.MediaType;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import utils.BaseApi;
-import utils.UserFactory;
-
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import static utils.UserFactory.positiveUser;
 
 public class RegistrationApiTests implements BaseApi {
+    Response response;
+
     @Test
     public void registrationPositiveApiTest() {
         User user = positiveUser();
-        System.out.println(user);
+        response = getResponse(user);
+        Assert.assertEquals(response.code(), 200);
+    }
+
+    @Test
+    public void registrationNegativeApiTest_WrongPassword() {
+        User user = positiveUser();
+        user.setPassword("sdsds");
+        response = getResponse(user);
+        Assert.assertEquals(response.code(), 400);
+    }
+
+    @Test
+    public void registrationNegativeApiTest_WrongEmail() {
+        User user = positiveUser();
+        user.setUsername("mailmail.ru");
+        response = getResponse(user);
+        Assert.assertEquals(response.code(), 400);
+    }
+    @Test
+    public void registrationNegativeApiTest_EmptyFields() {
+        User user = new User(" ", " ");
+        response = getResponse(user);
+        Assert.assertEquals(response.code(), 400);
+    }
+
+    @Test
+    public void registrationNegativeApiTest_UserAlreadyExists() {
+        User user = positiveUser();
+        response = getResponse(user);
+        response = getResponse(user);
+        Assert.assertEquals(response.code(), 409);
+    }
+
+    private Response getResponse(User user) {
         RequestBody requestBody = RequestBody.create(GSON.toJson(user), JSON);
         Request request = new Request.Builder()
                 .url(BASE_URL + REGISTRATION_URL)
                 .post(requestBody)
                 .build();
 
-        Response response;
         try {
             response = OK_HTTP_CLIENT.newCall(request).execute();
         } catch (
                 IOException e) {
             throw new RuntimeException();
         }
-     //   System.out.println(response.code());
-        Assert.assertEquals(response.code(), 200);
+        return response;
     }
     @Test
-    public void registrationNegativeApiTest() {
-        User user = positiveUser();
-        user.setPassword("sdsds");
-        RequestBody requestBody = RequestBody.create(GSON.toJson(user), JSON);
+    public void registrationNegativeApiTest_InvalidJSONStructure() {
+        Map<String, String> invalidUser = new HashMap<>();
+        invalidUser.put("usrname", "test@gmail.com");
+        invalidUser.put("pass", "QWEasd123!");
+
+        RequestBody requestBody = RequestBody.create(GSON.toJson(invalidUser), JSON);
         Request request = new Request.Builder()
                 .url(BASE_URL + REGISTRATION_URL)
                 .post(requestBody)
                 .build();
-
-        Response response;
         try {
             response = OK_HTTP_CLIENT.newCall(request).execute();
         } catch (
@@ -54,5 +87,52 @@ public class RegistrationApiTests implements BaseApi {
             throw new RuntimeException();
         }
         Assert.assertEquals(response.code(), 400);
+    }
+    @Test
+    public void registrationNegativeApiTest_EmptyJSON() {
+        RequestBody requestBody = RequestBody.create(GSON.toJson(""), JSON);
+        Request request = new Request.Builder()
+                .url(BASE_URL + REGISTRATION_URL)
+                .post(requestBody)
+                .build();
+        try {
+            response = OK_HTTP_CLIENT.newCall(request).execute();
+        } catch (
+                IOException e) {
+            throw new RuntimeException();
+        }
+        Assert.assertEquals(response.code(), 400);
+    }
+    @Test
+    public void registrationNegativeApiTest_NotJSON() {
+        User user = positiveUser();
+        RequestBody requestBody = RequestBody.create(GSON.toJson(user), MediaType.get("text/plain"));
+        Request request = new Request.Builder()
+                .url(BASE_URL + REGISTRATION_URL)
+                .post(requestBody)
+                .build();
+        try {
+            response = OK_HTTP_CLIENT.newCall(request).execute();
+        } catch (
+                IOException e) {
+            throw new RuntimeException();
+        }
+        Assert.assertEquals(response.code(), 400);
+    }
+    @Test
+    public void registrationNegativeApiTest_WrongEndpoint() {
+        User user = positiveUser();
+        RequestBody requestBody = RequestBody.create(GSON.toJson(user), MediaType.get("text/plain"));
+        Request request = new Request.Builder()
+                .url(BASE_URL + REGISTRATION_URL + "s")
+                .post(requestBody)
+                .build();
+        try {
+            response = OK_HTTP_CLIENT.newCall(request).execute();
+        } catch (
+                IOException e) {
+            throw new RuntimeException();
+        }
+        Assert.assertEquals(response.code(), 403);
     }
 }
